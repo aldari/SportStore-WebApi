f3df0d1637fc0621f462b6c157d492f1b1ef029e
El'dar Ismagilov <eldarismagilov@gmail.com>
2016-03-01 15:03:21 +0500

    Creating a RESTful Web Service

---
+++ SportStore/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using SportsStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SportStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public ProductsController()
+        {
+            Repository = new ProductRepository();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return Repository.Products;
+        }
+
+        public Product GetProduct(int id)
+        {
+            return Repository.Products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public async Task PostProduct(Product product)
+        {
+            await Repository.SaveProductAsync(product);
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            await Repository.DeleteProductAsync(id);
+        }
+
+        private IRepository Repository { get; set; }
+    }
+}
