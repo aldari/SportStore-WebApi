b7807d7e7e1f069c24f620b77146e8e62f76769a
El'dar Ismagilov <eldarismagilov@gmail.com>
2016-03-01 10:24:01 +0500

    Adding the Configuration Statements

--- SportStore/App_Start/IdentityConfig.cs
+++ SportStore/App_Start/IdentityConfig.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using SportStore.Infrastructure.Identity;
 
-[assembly: OwinStartup(typeof(SportsStore.IdentityConfig))]
-namespace SportsStore
+[assembly: OwinStartup(typeof(SportStore.IdentityConfig))]
+namespace SportStore
 {
     public class IdentityConfig
-    {public void Configuration(IAppBuilder app) { }
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<StoreIdentityDbContext>(
+            StoreIdentityDbContext.Create); app.CreatePerOwinContext<StoreUserManager>(StoreUserManager.Create);
+            app.CreatePerOwinContext<StoreRoleManager>(StoreRoleManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+            });
+        }
     }
 }
